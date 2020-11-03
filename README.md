2fd55ee2f426d50d73f5dd2d31e7e8f4eca0a603
AYA-kurokiri09 <ice6e63@gmail.com>
2020-11-03 16:40:45 +0900

    一時的にindex.php以外を削除

--- README.md
+++
@@ -1,2 +0,0 @@
-# SES-Joint-Development
-Study-English-Site-for-Engineers（SE2）
--- js/main.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-	setTimeout(function(){
-		$('.start-inner').fadeIn(1600);
-	},500); //0.5秒後にロゴをフェードイン!
-	setTimeout(function(){
-		$('.start').fadeOut(500);
-	},1500); //1.5秒後にロゴ含め真っ白背景をフェードアウト！
-});
